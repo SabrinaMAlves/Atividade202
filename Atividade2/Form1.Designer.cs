205dcc5a19d8cd18521b6fe96685dc09f27aa183
leonardo.palmeida3 <leonardo.palmeida3@OSA0625259W10-1>
2023-02-16 10:21:05 -0300

    Adicionar arquivos de projeto.

---
+++ Atividade2/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿
+namespace Atividade2
+{
+    partial class Media
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.listBox6 = new System.Windows.Forms.ListBox();
+            this.listBox7 = new System.Windows.Forms.ListBox();
+            this.listBox8 = new System.Windows.Forms.ListBox();
+            this.listBox9 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(871, 392);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(171, 49);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "LIMPAR";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(463, 408);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(178, 20);
+            this.txtNumero.TabIndex = 10;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(111, 32);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(224, 342);
+            this.listBox1.TabIndex = 11;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(383, 32);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(59, 342);
+            this.listBox2.TabIndex = 12;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(463, 32);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(61, 342);
+            this.listBox3.TabIndex = 13;
+            this.listBox3.SelectedIndexChanged += new System.EventHandler(this.listBox3_SelectedIndexChanged);
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(554, 32);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(65, 342);
+            this.listBox4.TabIndex = 14;
+            this.listBox4.SelectedIndexChanged += new System.EventHandler(this.listBox4_SelectedIndexChanged);
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.Location = new System.Drawing.Point(649, 33);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(63, 342);
+            this.listBox5.TabIndex = 15;
+            this.listBox5.SelectedIndexChanged += new System.EventHandler(this.listBox5_SelectedIndexChanged);
+            // 
+            // listBox6
+            // 
+            this.listBox6.FormattingEnabled = true;
+            this.listBox6.Location = new System.Drawing.Point(736, 33);
+            this.listBox6.Name = "listBox6";
+            this.listBox6.Size = new System.Drawing.Size(61, 342);
+            this.listBox6.TabIndex = 16;
+            this.listBox6.SelectedIndexChanged += new System.EventHandler(this.listBox6_SelectedIndexChanged);
+            // 
+            // listBox7
+            // 
+            this.listBox7.FormattingEnabled = true;
+            this.listBox7.Location = new System.Drawing.Point(829, 32);
+            this.listBox7.Name = "listBox7";
+            this.listBox7.Size = new System.Drawing.Size(61, 342);
+            this.listBox7.TabIndex = 17;
+            this.listBox7.SelectedIndexChanged += new System.EventHandler(this.listBox7_SelectedIndexChanged);
+            // 
+            // listBox8
+            // 
+            this.listBox8.FormattingEnabled = true;
+            this.listBox8.Location = new System.Drawing.Point(918, 32);
+            this.listBox8.Name = "listBox8";
+            this.listBox8.Size = new System.Drawing.Size(61, 342);
+            this.listBox8.TabIndex = 18;
+            this.listBox8.SelectedIndexChanged += new System.EventHandler(this.listBox8_SelectedIndexChanged);
+            // 
+            // listBox9
+            // 
+            this.listBox9.FormattingEnabled = true;
+            this.listBox9.Location = new System.Drawing.Point(996, 32);
+            this.listBox9.Name = "listBox9";
+            this.listBox9.Size = new System.Drawing.Size(61, 342);
+            this.listBox9.TabIndex = 19;
+            this.listBox9.SelectedIndexChanged += new System.EventHandler(this.listBox9_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(302, 406);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 20);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "MEDIA FINAL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(170, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 20);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "ALUNOS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(393, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 20);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "N1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(480, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 20);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "N2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(566, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 20);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "N3";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(662, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 20);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "N4";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(747, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 20);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "N5";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(842, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(31, 20);
+            this.label8.TabIndex = 27;
+            this.label8.Text = "N6";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(926, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(31, 20);
+            this.label9.TabIndex = 28;
+            this.label9.Text = "N7";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(1011, 9);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(31, 20);
+            this.label10.TabIndex = 29;
+            this.label10.Text = "N8";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(666, 392);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 49);
+            this.button1.TabIndex = 30;
+            this.button1.Text = "CALCULAR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Media
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PowderBlue;
+            this.ClientSize = new System.Drawing.Size(1093, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox9);
+            this.Controls.Add(this.listBox8);
+            this.Controls.Add(this.listBox7);
+            this.Controls.Add(this.listBox6);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.button2);
+            this.Name = "Media";
+            this.Text = "Media";
+            this.Load += new System.EventHandler(this.Media_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.ListBox listBox6;
+        private System.Windows.Forms.ListBox listBox7;
+        private System.Windows.Forms.ListBox listBox8;
+        private System.Windows.Forms.ListBox listBox9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Atividade2/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Atividade2
+{
+    public partial class Media : Form
+    {
+        SqlConnection conn = new SqlConnection("Data Source=OSA0625259W10-1;Initial Catalog=Media_alunos;Integrated Security=True"); //  > CON variavel para conexão
+        SqlCommand comando = new SqlCommand(); // Comando > Variavel para comando 
+        SqlDataReader dr;  // Consultar os dados 
+
+
+        public Media()
+        {
+
+            InitializeComponent();
+        }
+
+        private void Media_Load(object sender, EventArgs e)
+        {
+            comando.Connection = conn;  // Conectando o formulario com o banco de dados 
+            CarregarMedias();
+        }
+
+
+        private void CarregarMedias()
+        {
+
+            conn.Open(); //Executar abertura do banco de dados  
+            comando.CommandText = "select* from Media "; // comando para executar as informações de MEDIAS 
+            dr = comando.ExecuteReader(); // Irá executar a variavel comando
+
+            if (dr.HasRows) // se estiver linha 
+            {
+
+                while (dr.Read()) // Lopping // percorrer 
+
+                {
+                    listBox1.Items.Add(dr[0].ToString()); // Se inicia em zero listbox 
+                    listBox2.Items.Add(dr[1].ToString());
+                    listBox3.Items.Add(dr[2].ToString());
+                    listBox4.Items.Add(dr[3].ToString());
+                    listBox5.Items.Add(dr[4].ToString());
+                    listBox6.Items.Add(dr[5].ToString());
+                    listBox7.Items.Add(dr[6].ToString());
+                    listBox8.Items.Add(dr[7].ToString());
+                    listBox9.Items.Add(dr[8].ToString());
+                }
+
+
+
+            }
+            conn.Close(); // fechar banco de dados 
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox ler = sender as ListBox; // l Variavel 
+            if (ler.SelectedIndex != -1) //irá setar a linha toda selecionada 
+
+            {
+                listBox1.SelectedIndex = ler.SelectedIndex;
+                listBox2.SelectedIndex = ler.SelectedIndex;
+                listBox3.SelectedIndex = ler.SelectedIndex;
+                listBox4.SelectedIndex = ler.SelectedIndex;
+                listBox5.SelectedIndex = ler.SelectedIndex;
+                listBox6.SelectedIndex = ler.SelectedIndex;
+                listBox7.SelectedIndex = ler.SelectedIndex;
+                listBox8.SelectedIndex = ler.SelectedIndex;
+                listBox9.SelectedIndex = ler.SelectedIndex;
+            }
+            
+        }
+
+        private void listBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox ler = sender as ListBox;
+            if (ler.SelectedIndex != -1)
+
+            {
+                listBox1.SelectedIndex = ler.SelectedIndex;
+                listBox2.SelectedIndex = ler.SelectedIndex;
+                listBox3.SelectedIndex = ler.SelectedIndex;
+                listBox4.SelectedIndex = ler.SelectedIndex;
+                listBox5.SelectedIndex = ler.SelectedIndex;
+                listBox6.SelectedIndex = ler.SelectedIndex;
+                listBox7.SelectedIndex = ler.SelectedIndex;
+                listBox8.SelectedIndex = ler.SelectedIndex;
+                listBox9.SelectedIndex = ler.SelectedIndex;
+            }
+        }
+
+        private void listBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox ler = sender as ListBox;
+            if (ler.SelectedIndex != -1)
+
+            {
+                listBox1.SelectedIndex = ler.SelectedIndex;
+                listBox2.SelectedIndex = ler.SelectedIndex;
+                listBox3.SelectedIndex = ler.SelectedIndex;
+                listBox4.SelectedIndex = ler.SelectedIndex;
+                listBox5.SelectedIndex = ler.SelectedIndex;
+                listBox6.SelectedIndex = ler.SelectedIndex;
+                listBox7.SelectedIndex = ler.SelectedIndex;
+                listBox8.SelectedIndex = ler.SelectedIndex;
+                listBox9.SelectedIndex = ler.SelectedIndex;
+            }
+        }
+
+        private void listBox5_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox ler = sender as ListBox;
+            if (ler.SelectedIndex != -1)
+
+            {
+                listBox1.SelectedIndex = ler.SelectedIndex;
+                listBox2.SelectedIndex = ler.SelectedIndex;
+                listBox3.SelectedIndex = ler.SelectedIndex;
+                listBox4.SelectedIndex = ler.SelectedIndex;
+                listBox5.SelectedIndex = ler.SelectedIndex;
+                listBox6.SelectedIndex = ler.SelectedIndex;
+                listBox7.SelectedIndex = ler.SelectedIndex;
+                listBox8.SelectedIndex = ler.SelectedIndex;
+                listBox9.SelectedIndex = ler.SelectedIndex;
+            }
+        }
+
+        private void listBox6_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox ler = sender as ListBox;
+            if (ler.SelectedIndex != -1)
+
+            {
+                listBox1.SelectedIndex = ler.SelectedIndex;
+                listBox2.SelectedIndex = ler.SelectedIndex;
+                listBox3.SelectedIndex = ler.SelectedIndex;
+                listBox4.SelectedIndex = ler.SelectedIndex;
+                listBox5.SelectedIndex = ler.SelectedIndex;
+                listBox6.SelectedIndex = ler.SelectedIndex;
+                listBox7.SelectedIndex = ler.SelectedIndex;
+                listBox8.SelectedIndex = ler.SelectedIndex;
+                listBox9.SelectedIndex = ler.SelectedIndex;
+            }
+        }
+
+        private void listBox7_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox ler = sender as ListBox;
+            if (ler.SelectedIndex != -1)
+
+            {
+                listBox1.SelectedIndex = ler.SelectedIndex;
+                listBox2.SelectedIndex = ler.SelectedIndex;
+                listBox3.SelectedIndex = ler.SelectedIndex;
+                listBox4.SelectedIndex = ler.SelectedIndex;
+                listBox5.SelectedIndex = ler.SelectedIndex;
+                listBox6.SelectedIndex = ler.SelectedIndex;
+                listBox7.SelectedIndex = ler.SelectedIndex;
+                listBox8.SelectedIndex = ler.SelectedIndex;
+                listBox9.SelectedIndex = ler.SelectedIndex;
+            }
+        }
+
+        private void listBox8_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox ler = sender as ListBox;
+            if (ler.SelectedIndex != -1)
+
+            {
+                listBox1.SelectedIndex = ler.SelectedIndex;
+                listBox2.SelectedIndex = ler.SelectedIndex;
+                listBox3.SelectedIndex = ler.SelectedIndex;
+                listBox4.SelectedIndex = ler.SelectedIndex;
+                listBox5.SelectedIndex = ler.SelectedIndex;
+                listBox6.SelectedIndex = ler.SelectedIndex;
+                listBox7.SelectedIndex = ler.SelectedIndex;
+                listBox8.SelectedIndex = ler.SelectedIndex;
+                listBox9.SelectedIndex = ler.SelectedIndex;
+            }
+        }
+
+        private void listBox9_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox ler = sender as ListBox;
+            if (ler.SelectedIndex != -1)
+
+            {
+                listBox1.SelectedIndex = ler.SelectedIndex;
+                listBox2.SelectedIndex = ler.SelectedIndex;
+                listBox3.SelectedIndex = ler.SelectedIndex;
+                listBox4.SelectedIndex = ler.SelectedIndex;
+                listBox5.SelectedIndex = ler.SelectedIndex;
+                listBox6.SelectedIndex = ler.SelectedIndex;
+                listBox7.SelectedIndex = ler.SelectedIndex;
+                listBox8.SelectedIndex = ler.SelectedIndex;
+                listBox9.SelectedIndex = ler.SelectedIndex;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Calcular") 
+                
+                try
+                {
+                    int media = int.Parse(txtNumero.Text);
+                    listBox1.Items.Add(media);
+                    txtNumero.Text = " ";
+                }
+
+                catch
+                {
+                    MessageBox.Show(txtNumero.ToString());
+                }
+            
+            // MessageBox.Show(listBox1.SelectedItem.ToString());
+            // Media media = new Media();
+            // media = (listBox2 + listBox3 + listBox4 + listBox5 + listBox6 + listBox7 + listBox8 +listBox9);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtNumero.Clear();
+        }
+    }
+}
